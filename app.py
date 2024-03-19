c2d296ef2e72eb175ff8473c37c26ad2220e810d
Dewarshi Shukla <86153335+Dewrshi@users.noreply.github.com>
2024-03-20 01:44:35 +0530

    added api key and updated requirement document

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, request, jsonify
+from langchain.prompts import PromptTemplate
+import openai
+import os
+import time
+
+secret_key = os.environ.get("openai_flask_bot_api")
 
 app= Flask(__name__)
 
