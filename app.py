58d2b1aabf3647feefee61bbff484ded0d1149c2
Dewarshi Shukla <86153335+Dewrshi@users.noreply.github.com>
2024-03-19 19:32:33 +0530

    Added Chatbot form at homepage template

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app= Flask(__name__)
 @app.route("/")
 def home():
     return render_template('home.html')
-@app.route("/")
+@app.route("/index")
 def index():
     return render_template('index.html')
 
